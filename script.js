b0cdf46b73993019edc9d292f0f1182909b70a8d
Ruby-YSY <56031438+Ruby-YSY@users.noreply.github.com>
2026-02-09 01:39:53 +0000

    Improved readability and optimized the code

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function getComputerChoice() {
-  // let array = ["Rock", "Paper", "Scissors"];    || A possible solution with using arrays
-  // return array[Math.floor(Math.random() * 3)];
-
   switch (Math.floor(Math.random() * 3)) {
     case 0:
       return "Rock";
@@ -27,31 +24,41 @@ let computerScore = 0;
 function playRound(computerChoice, humanChoice) {
   const computerSelection = computerChoice;
   const humanSelection =
-    humanChoice.at(0).toUpperCase() + humanChoice.substr(1);
+    humanChoice.at(0).toUpperCase() + humanChoice.substr(1).toLowerCase();
+
+  function result(isHumanWinner) {
+    if (isHumanWinner) {
+      console.log(`You've won! ${humanSelection} beats ${computerSelection}.`);
+      humanScore++;
+    } else {
+      console.log(`You've lost! ${computerSelection} beats ${humanSelection}.`);
+      computerScore++;
+    }
+  }
 
   console.log(humanSelection);
   console.log(computerSelection);
 
-  if (humanSelection === "Rock" && computerSelection === "Scissors") {
-    console.log(`You've won! ${humanSelection} beats ${computerSelection}.`);
-    humanScore++;
-  } else if (humanSelection === "Rock" && computerSelection === "Paper") {
-    console.log(`You've lost! ${computerSelection} beats ${humanSelection}.`);
-    computerScore++;
-  } else if (humanSelection === "Paper" && computerSelection === "Rock") {
-    console.log(`You've won! ${humanSelection} beats ${computerSelection}.`);
-    humanScore++;
-  } else if (humanSelection === "Paper" && computerSelection === "Scissors") {
-    console.log(`You've lost! ${computerSelection} beats ${humanSelection}.`);
-    computerScore++;
-  } else if (humanSelection === "Scissors" && computerSelection === "Paper") {
-    console.log(`You've won! ${humanSelection} beats ${computerSelection}.`);
-    humanScore++;
-  } else if (humanSelection === "Scissors" && computerSelection === "Rock") {
-    console.log(`You've lost! ${computerSelection} beats ${humanSelection}.`);
-    computerScore++;
-  } else {
+  if (humanSelection === computerSelection) {
     console.log("Draw!");
+  } else if (humanSelection === "Rock") {
+    if (computerSelection === "Scissors") {
+      result(true);
+    } else {
+      result(false);
+    }
+  } else if (humanSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      result(true);
+    } else {
+      result(false);
+    }
+  } else if (humanSelection === "Scissors") {
+    if (computerSelection === "Paper") {
+      result(true);
+    } else {
+      result(false);
+    }
   }
 }
 
