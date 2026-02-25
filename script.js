df3b1a01ba4bb505ce315c00e319acfb74f557c0
Ruby-YSY <56031438+Ruby-YSY@users.noreply.github.com>
2026-02-25 12:27:03 +0000

    Removed unused break commands in the switch...case construction

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ function getComputerChoice() {
   switch (Math.floor(Math.random() * 3)) {
     case 0:
       return "Rock";
-      break;
 
     case 1:
       return "Paper";
-      break;
 
     case 2:
       return "Scissors";
-      break;
   }
 }
 
